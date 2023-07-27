92e3726f3e57860c248f3e58c83a970052a086ce
rencity.vn <rencity.vn@gmail.com>
2023-07-27 09:39:35 +0700

    add code

---
+++ lib/const/color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const primaryColor = Color(0xFFEF4355);
+
+class AppColor {
+  static const Color diabled = Color(0xFFC7C7C7);
+  static const Color blue = Color(0xFF084DF0);
+  static const Color textColor = Color(0xFF212121);
+  static const Color primaryColor = Color(0xFFFB3E15);
+  static const Color divider= Color(0xFFBFBFBF);
+}
